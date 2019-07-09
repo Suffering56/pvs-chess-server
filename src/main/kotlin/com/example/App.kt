3612002a070dffb2700ab8b5573f932449f243e1
v.peschanyi <p.v.s.oren@gmail.com>
2019-07-09 22:25:42 +0300

    - init spring-rest;

--- src/main/kotlin/com/example/App.kt
+++ src/main/kotlin/com/example/App.kt
@@ -1,5 +1,27 @@
 package com.example
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class App : CommandLineRunner {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    companion object {
+        fun getVersion(): String {
+            return "1.0.1"
+        }
+    }
+
+    override fun run(vararg args: String?) {
+        log.info("APP_VERSION = ${getVersion()}")
+    }
 }
+
+fun main(args: Array<String>) {
+    SpringApplication.run(App::class.java, *args)
+}
+
