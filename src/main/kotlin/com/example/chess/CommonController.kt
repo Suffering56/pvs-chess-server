87996c3ecb4f024cf4bb9984647f36a21a87fc63
v.peschaniy <v.peschaniy@corp.mail.ru>
2019-07-15 18:33:01 +0300

    - test java autowiring

--- src/main/kotlin/com/example/chess/CommonController.kt
+++ src/main/kotlin/com/example/chess/CommonController.kt
@@ -1,5 +1,7 @@
 package com.example.chess
 
+import com.example.chess.entity.GameRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 class CommonController {
 
+    @Autowired
+    lateinit var gameRepository: GameRepository
+
     @GetMapping("/version")
     fun get(): ResponseEntity<String> {
-        return ResponseEntity.ok(App.getVersion())
+        val count = gameRepository.findAll().stream().count()
+        return ResponseEntity.ok("version=${App.getVersion()}, count=$count")
     }
 }
