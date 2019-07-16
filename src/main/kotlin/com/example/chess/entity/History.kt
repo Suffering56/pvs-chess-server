a093694f40bc400b82de78ab0bc69965f328cda0
v.peschaniy <v.peschaniy@corp.mail.ru>
2019-07-16 18:43:27 +0300

    - working..

---
+++ src/main/kotlin/com/example/chess/entity/History.kt
@@ -0,0 +1,75 @@
+package com.example.chess.entity
+
+import com.example.chess.enums.PieceType
+import org.hibernate.annotations.GenericGenerator
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+/**
+ * @author v.peschaniy
+ *      Date: 16.07.2019
+ */
+@Entity
+data class History(
+
+    @Id
+    @GenericGenerator(
+        name = "history_id_seq",
+        strategy = "sequence-identity",
+        parameters = [org.hibernate.annotations.Parameter(name = "sequence", value = "history_id_seq")]
+    )
+    @GeneratedValue(generator = "history_id_seq")
+    var id: Long?,
+
+    @Column(nullable = false)
+    val gameId: Long,
+
+    @Column(nullable = false)
+    val position: Int,
+
+    @Column(nullable = false)
+    val rowIndexFrom: Int,
+
+    @Column(nullable = false)
+    val columnIndexFrom: Int,
+
+    @Column(nullable = false)
+    val rowIndexTo: Int,
+
+    @Column(nullable = false)
+    val columnIndexTo: Int,
+
+    @Column(nullable = true)
+    val pieceFromPawn: PieceType?,
+
+    @Column
+    val description: String?
+) {
+
+
+//    val pointFrom: PointDTO
+//        @Transient
+//        get() = PointDTO.valueOf(rowIndexFrom, columnIndexFrom)
+//
+//    val pointTo: PointDTO
+//        @Transient
+//        get() = PointDTO.valueOf(rowIndexTo, columnIndexTo)
+//
+//    val formattedPosition: String
+//        @Transient
+//        get() = String.format("%02d", position)
+//
+//    @Transient
+//    fun toExtendedMove(matrix: CellsMatrix): ExtendedMove {
+//        val from = matrix.getCell(pointFrom)
+//        val to = matrix.getCell(pointTo)
+//        return ExtendedMove(from, to)
+//    }
+//
+//    @Transient
+//    fun toReadableString(): String {
+//        return pointFrom + "-" + pointTo
+//    }
+}
--- src/main/kotlin/com/example/chess/web/CommonController.kt
+++ src/main/kotlin/com/example/chess/web/CommonController.kt
@@ -3,7 +3,6 @@ package com.example.chess.web
 import com.example.chess.App
 import com.example.chess.repository.GameRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -13,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController
 class CommonController @Autowired constructor(private val gameRepository: GameRepository) {
 
     @GetMapping("/version")
-    fun get(): ResponseEntity<String> {
+    fun get(): String {
         val count = gameRepository.findAll().stream().count()
-        return ResponseEntity.ok("version=${App.getVersion()}, count=$count")
+        return "version=${App.getVersion()}, count=$count"
     }
 }
